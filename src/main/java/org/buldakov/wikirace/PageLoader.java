5d075c86a5ab196b19a223f6625b10e6e2c5d7a6
Victor Buldakov <victor@buldakov.org>
2017-03-06 23:36:47 +0100

    add bidirectional traversal

--- src/main/java/org/buldakov/wikirace/PageLoader.java
+++ src/main/java/org/buldakov/wikirace/PageLoader.java
@@ -1,5 +1,8 @@
 package org.buldakov.wikirace;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -13,6 +16,9 @@ import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class PageLoader {
@@ -25,6 +31,7 @@ public class PageLoader {
     private final HttpUrl endpoint;
     private final LinkExtractor extractor;
     private final boolean verbose;
+    private final LoadingCache<String, String> pages;
 
     public PageLoader(String baseUri, List<String> excludePrefixes, boolean verbose) {
         this.baseUri = baseUri;
@@ -33,40 +40,30 @@ public class PageLoader {
         this.client = new OkHttpClient();
         this.endpoint = HttpUrl.parse(baseUri);
         this.extractor = new LinkExtractor();
+        this.pages = CacheBuilder.newBuilder()
+                .maximumSize(1000)
+                .expireAfterAccess(1, TimeUnit.MINUTES)
+                .build(new CacheLoader<String, String>() {
+                    @Override
+                    public String load(String key) throws Exception {
+                        return loadPage(key);
+                    }
+                });
     }
 
-    public List<String> getPaths(String path) {
-        Request request = new Request.Builder()
-                .url(endpoint.newBuilder().addPathSegments(StringUtils.removeStart(path, "/")).build())
-                .build();
-
+    public Set<String> getPaths(String path) {
         try {
-
-            StopWatch watch = new StopWatch();
-            watch.start();
-
-            Response response = client.newCall(request).execute();
-
-            watch.stop();
-            if (verbose) {
-                logger.info("Download the resource {} {}ms", path, watch.getTime());
-            }
-            if (!response.isSuccessful()) {
-                logger.error("Impossible to load resource {} {}", path, response);
-                response.close();
-                return Collections.emptyList();
-            }
-            String body = response.body().string();
+            String body = pages.get(path);
 
             return extractor.getLinks(body, baseUri)
                     .stream()
                     .map(HttpUrl::parse)
                     .filter(this::filterLink)
                     .map(url -> String.join("/", url.pathSegments()))
-                    .collect(Collectors.toList());
-        } catch (IOException e) {
+                    .collect(Collectors.toSet());
+        } catch (ExecutionException e) {
             logger.error("Impossible to load resource {}", path);
-            return Collections.emptyList();
+            return Collections.emptySet();
         }
     }
 
@@ -77,6 +74,27 @@ public class PageLoader {
                 && url.host().equals(endpoint.host())
                 && url.port() == endpoint.port()
                 && excludePrefixes.stream().noneMatch(prefix -> StringUtils.startsWith(url.encodedPath(), prefix));
+    }
 
+    private String loadPage(String path) throws IOException {
+        Request request = new Request.Builder()
+                .url(endpoint.newBuilder().addPathSegments(StringUtils.removeStart(path, "/")).build())
+                .build();
+
+        StopWatch watch = new StopWatch();
+        watch.start();
+
+        Response response = client.newCall(request).execute();
+
+        watch.stop();
+        if (verbose) {
+            logger.info("Download the resource {} {}ms", path, watch.getTime());
+        }
+        if (!response.isSuccessful()) {
+            logger.error("Impossible to load resource {} {}", path, response);
+            response.close();
+            throw new IllegalStateException("Impossible to load resource");
+        }
+        return response.body().string();
     }
 }
--- src/main/java/org/buldakov/wikirace/WebsiteTraversor.java
+++ src/main/java/org/buldakov/wikirace/WebsiteTraversor.java
@@ -1,11 +1,6 @@
 package org.buldakov.wikirace;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.Set;
+import java.util.*;
 
 public class WebsiteTraversor {
 
@@ -16,23 +11,79 @@ public class WebsiteTraversor {
     }
 
     public List<String> traverse(String from, String to) {
-        Set<String> visited = new HashSet<>();
-        Queue<String> queue = new LinkedList<>();
-        queue.offer(from);
-        visited.add(from);
-        while (!queue.isEmpty()) {
-            String value = queue.remove();
-            if (value.equals(to)) {
-                return Collections.emptyList();
+        Map<String, String> visitedFrom = new HashMap<>();
+        Map<String, String> visitedTo = new HashMap<>();
+        Queue<String> queueFrom = new LinkedList<>();
+        Queue<String> queueTo = new LinkedList<>();
+        queueFrom.offer(from);
+        queueTo.offer(to);
+        visitedFrom.put(from, "");
+        visitedTo.put(to, "");
+        while (!visitedFrom.isEmpty()) {
+
+            Optional<String> middle = visitFrom(queueFrom, visitedFrom, visitedTo);
+            if (middle.isPresent()) {
+                return buildPath(middle.get(), visitedFrom, visitedTo);
             }
-            List<String> paths = pageLoader.getPaths(value);
-            for (String path : paths) {
-                if (!visited.contains(path)) {
-                    queue.offer(path);
-                    visited.add(path);
-                }
+            middle = visitTo(queueTo, visitedFrom, visitedTo);
+            if (middle.isPresent()) {
+                return buildPath(middle.get(), visitedFrom, visitedTo);
             }
         }
         return Collections.emptyList();
+    }
+
+    private List<String> buildPath(String middle, Map<String, String> visitedFrom, Map<String, String> visitedTo) {
+        LinkedList<String> result = new LinkedList<>();
+        result.add(middle);
+
+        String current = middle;
+        while (visitedFrom.get(current) != null && !visitedFrom.get(current).equals("")) {
+            current = visitedFrom.get(current);
+            result.addFirst(current);
+        }
+        current = middle;
+        while (visitedTo.get(current) != null && !visitedTo.get(current).equals("")) {
+            current = visitedTo.get(current);
+            result.add(current);
+        }
+        return result;
+    }
+
+    private Optional<String> visitFrom(Queue<String> queueFrom, Map<String, String> visitedFrom,
+                                       Map<String, String> visitedTo) {
+
+        String value = queueFrom.remove();
+        if (visitedTo.containsKey(value)) {
+            return Optional.of(value);
+        }
+        Set<String> paths = pageLoader.getPaths(value);
+        for (String path : paths) {
+            if (!visitedFrom.containsKey(path)) {
+                queueFrom.offer(path);
+                visitedFrom.put(path, value);
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    private Optional<String> visitTo(Queue<String> queueTo, Map<String, String> visitedFrom,
+                                     Map<String, String> visitedTo) {
+        String value = queueTo.remove();
+        if (visitedFrom.containsKey(value)) {
+            return Optional.of(value);
+        }
+        Set<String> paths = pageLoader.getPaths(value);
+        for (String path : paths) {
+            //we need to check that this page actually have links to the destination page, because
+            //links graph unidirectional.
+            if (!visitedTo.containsKey(path) && pageLoader.getPaths(path).contains(value)) {
+                queueTo.offer(path);
+                visitedTo.put(path, value);
+            }
+        }
+
+        return Optional.empty();
     }
 }
